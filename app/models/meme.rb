135a54ecd451083bbc88f13940a88fe770bf4ffa
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-28 13:23:50 -0500

    Partial validations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,3 +1,5 @@
 class Meme < ApplicationRecord
   belongs_to :category
+  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
-  has_many :memes
+  has_and_belongs_to_many :memes
   has_many :comments
 end
