413a7dc7f3291886918df76f6a4cf6768a42272c
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-28 22:33:11 -0500

    Added links to and from User page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :memes
 
+  devise_for :users
   root to: 'home#index'
-  resources :users, only: :show
+  get '/my_profile', to: 'users#show'
+  # resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
