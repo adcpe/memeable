815258822638cd3e7aa2bd813b7471ca235a5678
Josue Narro <josue.narro.a@gmail.com>
2020-05-29 17:26:58 -0500

    Create category route

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -61,6 +61,10 @@ class MemesController < ApplicationController
     end
   end
 
+  def categories
+    @meme_categ = Meme.all.group_by { |meme| meme.category.name }
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_meme
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   devise_for :users
   get '/my_profile', to: 'users#show'
+  get '/categories', to: 'memes#categories'
 end
