7ca975297893f287429d81899c03a92ba487a614
Joel Eche <joel.eche5@gmail.com>
2020-05-29 16:07:30 -0500

    Votes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'memes#index'
   resources :memes
   post '/comments/new', to: 'comments#create'
+  # Votes
+  post '/votes/new', to: 'votes#create'
+  post '/votes/unvote', to: 'votes#unvote'
 
   devise_for :users
   get '/my_profile', to: 'users#show'
