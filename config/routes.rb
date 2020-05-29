a9ceccbfb087b6be52c434682ad62258920f3599
Joel Eche <joel.eche5@gmail.com>
2020-05-29 15:07:44 -0500

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'memes#index'
   resources :memes
+  post '/comments/new', to: 'comments#create'
 
   devise_for :users
   get '/my_profile', to: 'users#show'
